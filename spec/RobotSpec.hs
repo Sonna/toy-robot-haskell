79f575c225f3d7932bd47040d1301b4fccd1b6fb
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-29 13:52:23 +1100

    Add a simple `new` function example
    Add a simple `new` function example that creates a new Robot Entity
    without any arguments, which makes a Robot at the default coordinates
    position and Cardinal facing direction.

--- spec/RobotSpec.hs
+++ spec/RobotSpec.hs
@@ -6,6 +6,10 @@ import Test.Hspec
 spec :: Spec
 spec = do
 
+  describe "Robot constructor" $ do
+    it "can be constructed without values" $ do
+      (report $ new() ) `shouldBe` "0,0,NORTH"
+
   describe "Validate report function" $ do
     it "report is supposed to return x, y and facing direction" $ do
       (report $ Robot 0 0 "NORTH" ) `shouldBe` "0,0,NORTH"
--- src/Robot.hs
+++ src/Robot.hs
@@ -2,12 +2,17 @@ module Robot
 (
   -- Robot (..),
   Entity (..),
+  new,
   report
 ) where
 
 -- data Robot = Coordinate Int Int String
 -- report :: Robot -> Int -> Int -> String
 data Entity = Robot Int Int String
+
+new :: () -> Entity
+new () = Robot 0 0 "NORTH"
+
 report :: Entity -> String
 report (Robot x y facing) = show x ++ "," ++ show y ++  "," ++ facing
 
