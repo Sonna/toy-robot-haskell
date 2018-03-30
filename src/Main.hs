52118b56fcf8f86ac1c62a19975e638bc84ff533
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-30 15:00:27 +1100

    Update `Main` to handle User input
    Update the Main script to handle User input, with a possibly of
    extend the processing of filenames given as command line arguments. This
    main loop processes User Input by first getting a line from the console,
    splitting that line on whitespaces into separate words and then uses
    pattern matching build the correct set of `command` & `commandArgs` that
    get passed directly into the Robot `exec` function, whilst returning the
    updated robot on subsequent iterations of the loop.
    
    == Notes:
    - There are no Loops in Haskell, only Recursion
    
    == References:
    - [Haskell/Libraries/IO \- Wikibooks, open books for an open world]
      (https://en.wikibooks.org/wiki/Haskell/Libraries/IO)
    
    - [io \- Haskell \- loop over user input \- Stack Overflow]
      (https://stackoverflow.com/questions/18573924/haskell-loop-over-user-input)
    
    - [Haskell \- "How can I use "if" statement in "do" block properly? \-
      Stack Overflow"]
      (https://stackoverflow.com/questions/5920888/haskell-how-can-i-use-if-statement-in-do-block-properly)
    
    - [Haskell where clause syntax inside a do block \- Stack Overflow]
      (https://stackoverflow.com/questions/14092801/haskell-where-clause-syntax-inside-a-do-block)
    
    - [Recursion \- Learn You a Haskell for Great Good\!]
      (http://learnyouahaskell.com/recursion)
    
    - [Haskell syntax for a case expression in a do block \- Stack Overflow]
      (https://stackoverflow.com/questions/156013/haskell-syntax-for-a-case-expression-in-a-do-block)
    
    - [haskell \- Stdin as IO Handle \- Stack Overflow]
      (https://stackoverflow.com/questions/3488328/stdin-as-io-handle)
    
    - [Yet Another Haskell Tutorial/Io \- Wikibooks, open books for an open
      world]
      (https://en.wikibooks.org/wiki/Yet_Another_Haskell_Tutorial/Io)
    
    - [Correct way of reading from stdin in haskell \- Stack Overflow]
      (https://stackoverflow.com/questions/29060916/correct-way-of-reading-from-stdin-in-haskell)
    
    - [haskell \- "User error" pattern matching failure when using getArgs
      \- Stack Overflow]
      (https://stackoverflow.com/questions/22052650/user-error-pattern-matching-failure-when-using-getargs)

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,35 @@
 module Main where
 
 import Robot
+import System.IO
+import System.Environment
+import Data.List
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
   let robot = new()
-  subject <- report robot
-  print subject
+
+  args <- getArgs
+  case args of
+      [file] -> withFile (head args) ReadMode (\handle -> do
+        contents <- hGetContents handle
+        putStr contents)
+      [] -> userLoop robot >> putStr ""
+      _ -> putStr "Do Nothing"
+
+userLoop :: Robot -> IO Robot
+userLoop robot = do
+  rawCommands <- readCommandLine
+  if ((head rawCommands) == "EXIT")
+    then putStr "" >> return robot -- Need to fix issue later
+    else do
+        let (command, commandArgs) = case rawCommands of
+              [rawCommand, rawCommandArgs] -> (rawCommand, rawCommandArgs)
+              [rawCommand] -> (rawCommand, "")
+              _ -> ("", "")
+
+        exec robot command commandArgs >>= \newRobot -> userLoop newRobot
+        -- putStr "" -- Need to fix issue later
+
+readCommandLine :: IO [String]
+readCommandLine = fmap (words) getLine
