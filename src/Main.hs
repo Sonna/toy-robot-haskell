3be66a6a4f894b794ab8eb26d6f24a9cd6d04d48
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-30 17:16:54 +1100

    Refactor Main script once more
    Refactor the Main script once more in order to pass the return argument
    with `>>=` operators anonymously as the last argument of the next
    function proceeding the operator. This helps reduce the number of one
    off use arguments and better utilizes Haskell language features.
    
    == References:
    - [Haskell/do notation \- Wikibooks, open books for an open world]
      (https://en.wikibooks.org/wiki/Haskell/do_notation#Notes)

--- src/Main.hs
+++ src/Main.hs
@@ -9,31 +9,31 @@ main :: IO ()
 main = do
   let robot = new()
 
-  args <- getArgs
-  case args of
-      [file] -> openFile (head args) ReadMode >>= \file -> fileLoop robot file
+  getArgs >>= \args -> case args of
+      [file] -> openFile (head args) ReadMode >>= \file -> fileLoop file robot
       [] -> userLoop robot
       _ -> putStr "Too many or not few enough Arguments" >> return robot
+
   putStr ""
 
-fileLoop :: Robot -> Handle -> IO Robot
-fileLoop robot file = hIsEOF file >>= \ineof ->
+fileLoop :: Handle -> Robot -> IO Robot
+fileLoop file robot = hIsEOF file >>= \ineof ->
   if ineof
     then putStr "" >> return robot
     else do
-      hGetLine file >>= \line ->
-        execCommand robot (words line) >>= \newRobot ->
-          fileLoop newRobot file
+      hGetLine file >>=
+        execCommand robot >>=
+          fileLoop file
 
 userLoop :: Robot -> IO Robot
 userLoop robot = getLine >>= \line ->
   case (words line) of
     ["EXIT"] -> putStr "" >> return robot
-    _ -> execCommand robot (words line) >>= \newRobot -> userLoop newRobot
+    _ -> execCommand robot line >>= userLoop
 
-execCommand :: Robot -> [String] -> IO Robot
-execCommand robot commandList = exec robot command commandArgs
-  where (command, commandArgs) = tuplifyInput commandList
+execCommand :: Robot -> String -> IO Robot
+execCommand robot line = exec robot command commandArgs
+  where (command, commandArgs) = tuplifyInput (words line)
 
 tuplifyInput :: [String] -> (String, String)
 tuplifyInput wordList =
