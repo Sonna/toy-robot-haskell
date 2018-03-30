38ff8858729ef0ee001c3fdc0fa03ecdcf794066
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-30 18:23:14 +1100

    Add some specifications tests for Toy Robot CLI
    Add some additional tests for the helper functions that curate the lines
    being feed into the Robot subject after each `getLine` call within
    either the User or File Loops. However, it is not known if it possible
    to feed a file in as `stdin` standard input, but File loop should still
    be testable.`

--- spec/ToyRobot/CLISpec.hs
+++ spec/ToyRobot/CLISpec.hs
@@ -1,8 +1,9 @@
 module ToyRobot.CLISpec where
 
+import Robot
 import ToyRobot.CLI
 import TestHelpers
--- import System.IO.Silently
+
 import Test.Hspec
 
 spec :: Spec
@@ -13,3 +14,48 @@ spec = do
       let subject = tuplifyInput ["Hello", "world"]
       subject `shouldBe` ("Hello", "world")
 
+    it "is tuplifys a list of 1 Strings or words" $ do
+      let subject = tuplifyInput ["Hello"]
+      subject `shouldBe` ("Hello", "")
+
+    it "is tuplifys a list of 3 Strings or words" $ do
+      let subject = tuplifyInput ["Hello", "world", "foobar"]
+      subject `shouldBe` ("", "")
+
+    it "is tuplifys a list of no Strings or words" $ do
+      let subject = tuplifyInput []
+      subject `shouldBe` ("", "")
+
+  describe "Validate execCommand function" $ do
+    it "updates a given robot when feed a line of text" $ do
+      let line = "REPORT"
+      let robot = Robot 0 0 "NORTH"
+
+      (output, subject) <- capture(execCommand robot line)
+
+      output `shouldBe` "0,0,NORTH\n"
+      (x subject) `shouldBe` 0
+      (y subject) `shouldBe` 0
+      (facing subject) `shouldBe` "NORTH"
+
+    it "updates a given robot when feed an unknown command in line of text" $ do
+      let line = "The quick brown fox jumps over the lazy dog"
+      let robot = Robot 0 0 "NORTH"
+
+      (output, subject) <- capture(execCommand robot line)
+
+      output `shouldBe` ""
+      (x subject) `shouldBe` 0
+      (y subject) `shouldBe` 0
+      (facing subject) `shouldBe` "NORTH"
+
+    it "updates a given robot when feed the PLACE command in line of text" $ do
+      let line = "PLACE 4,2,SOUTH"
+      let robot = Robot 0 0 "NORTH"
+
+      (output, subject) <- capture(execCommand robot line)
+
+      output `shouldBe` ""
+      (x subject) `shouldBe` 4
+      (y subject) `shouldBe` 2
+      (facing subject) `shouldBe` "SOUTH"
